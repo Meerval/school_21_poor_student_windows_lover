af7cdaab10b4497d49f5a3f5881ce0adbdbe7e09
Diena <66274095+Meerval@users.noreply.github.com>
2025-07-04 12:31:25 +0300

    Update docker_preset.sh

--- docker_preset.sh
+++ docker_preset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e '\n# Подсветка текущей Git-ветки в prompt\nparse_git_branch() {\n  branch=$(git branch 2>/dev/null | sed -n '\''/\* /s///p'\'');\n  if [ -n "$branch" ]; then echo " ($branch)"; fi\n}\nexport PS1='\''\[\e[32m\]\u@\h:\[\e[35m\]\w\[\e[33m\]$(parse_git_branch)\[\e[0m\] \$ '\''\n' >> ~/.bashrc && source ~/.bashrc
+USER="ashaclem"
 
 S21_DIR="$HOME/s21"
 SSH_SOURCE_DIR="$HOME/s21/.ssh"
@@ -12,7 +12,7 @@ KEY_TARGET="$SSH_TARGET_DIR/id_ed25519"
 KEY_SOURCE_PUB="$SSH_SOURCE_DIR/id_ed25519.pub"
 KEY_TARGET_PUB="$SSH_TARGET_DIR/id_ed25519.pub"
 
-sudo chown -R ashaclem:ashaclem $S21_DIR
+sudo chown -R $USER:$USER $S21_DIR
 sudo chmod -R u+rwX $S21_DIR
 
 if [ ! -d "$SSH_TARGET_DIR" ]; then
@@ -40,10 +40,10 @@ fi
 
 chmod 600 "$KEY_TARGET"
 chmod 644 "$KEY_TARGET_PUB"
-chown ashaclem:ashaclem "$KEY_TARGET" "$KEY_TARGET_PUB"
+chown $USER:$USER "$KEY_TARGET" "$KEY_TARGET_PUB"
 
-git config --global user.name ashaclem
-git config --global user.email ashaclem@student.21-school.ru
+git config --global user.name $USER
+git config --global user.email $USER@student.21-school.ru
 git config --global core.filemode false
 git config --global core.autocrlf input
 git config --global core.symlinks false
